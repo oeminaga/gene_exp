ec42653befeea5341c184977a0c7d1c82b6ea857
oeminaga <okyaz@gmx.de>
2018-08-02 04:44:06 -0700

    Adding function for generating patches

--- utils.py
+++ utils.py
@@ -726,7 +726,8 @@ class OpenSlideOnlivePatch:
         plt.imshow(level_3)
         plt.show()
         region = self.MaxRegion(regions)
-
+        plt.imshow(region)
+        plt.show()
         minr, minc, maxr, maxc = region.bbox
         minr_new = minr * level_factor
         minc_new = minc * level_factor
