4939e58eb70ce68a2619c1198bd30afffb073eb2
oeminaga <okyaz@gmx.de>
2018-08-01 18:24:38 -0700

    Adding function for generating patches

--- utils.py
+++ utils.py
@@ -886,17 +886,12 @@ class OpenSlideOnlivePatch:
             if percentage_positive > 0.90:
                 from skimage.filters import threshold_otsu
                 img = self.image.read_region((x,y),0,patch_size)
-                plt.imshow(img)
-                plt.show()
                 image = np.asarray(img)
                 image = image[:, :, 0:3]
                 image = skimage.color.rgb2grey(image)
-                plt.imshow(image)
-                plt.show()
                 thresh_min = threshold_otsu(image)
-                binary_min = mask_selected <= thresh_min
+                binary_min = image <= thresh_min
                 number_positive = np.count_nonzero(binary_min)
-
                 percentage_positive = number_positive / total_size
                 if percentage_positive > 0.90:
                     reg_lst.append([x, y])
