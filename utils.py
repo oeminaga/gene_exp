37b5947c2206d4ad820740692267ecb91b5e1be2
oeminaga <okyaz@gmx.de>
2018-08-02 20:41:44 -0700

    Adding function for generating patches

--- utils.py
+++ utils.py
@@ -689,6 +689,8 @@ class OpenSlideOnlivePatch:
         plt.show()
         from skimage.filters import threshold_minimum, threshold_local
         thresh_min = threshold_minimum(tissues)# , 35, offset=10)
+        if thresh_min>(-0.95):
+            thresh_min = (-1.3)
         binary_min = np.logical_and(tissues > thresh_min, tissues < (-0.95))
         binary_min = morphology.closing(binary_min, square(4))
         binary_min = morphology.opening(binary_min, square(4))
