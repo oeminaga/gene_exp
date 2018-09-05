2f71da3d506e99c506fad38b3b6d7ffb0bb6bb68
okyazeminaga <okyaz@gmx.de>
2018-09-04 21:53:36 -0700

    Adding function for generating patches

--- main.py
+++ main.py
@@ -306,11 +306,11 @@ if __name__ == "__main__":
     parser.add_argument('--lr', default=0.1, type=float,
                         help="Initial learning rate")
     parser.add_argument('--nb_class', '-n', default=3, type=int, help="the number of the classes")
-    parser.add_argument('--path_validation', default="/home/eminaga/GenHistomic/valid/",
+    parser.add_argument('--path_validation', default="/home/eminaga/GenHistomic/valid",
                         help="images for validation")
-    parser.add_argument('--path_train', default="/home/eminaga/GenHistomic/train/",
+    parser.add_argument('--path_train', default="/home/eminaga/GenHistomic/train",
                         help="images for training")
-    parser.add_argument('--path_test', default="/home/eminaga/GenHistomic/test/",
+    parser.add_argument('--path_test', default="/home/eminaga/GenHistomic/test",
                         help="images for test")
     parser.add_argument('--lr_decay', default=0.999, type=float,
                         help="The value multiplied by lr at each epoch. Set a larger value for larger epochs")
